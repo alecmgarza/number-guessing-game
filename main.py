862c3ff7a5af47c35ed9574baf3a647a5e0a730e
Alec Garza <alecmgarza@gmail.com>
2026-01-02 01:19:01 -0600

    Implement guess limit along with victory and game over messages

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import random
 
 random_num = random.randint(1,100)
-print(random_num)
+# print(random_num)
+guess_count = 5
 
-player_guess = int(input("Thinking of a number between 1 and 100..."))
+player_guess = int(input(f"Thinking of a number between 1 and 100...({guess_count} guesses remaining)\n"))
+guess_count = guess_count - 1
 
-if player_guess > random_num:
-    player_guess = int(input("Lower..."))
-else: 
-    player_guess = int(input("Higher"))
+while guess_count > 0:
+    if player_guess > random_num:
+        player_guess = int(input(f"Lower...({guess_count} guesses remaining)\n"))
+        guess_count = guess_count - 1
+    elif player_guess < random_num:
+        player_guess = int(input(f"Higher...({guess_count} guesses remaining)\n"))
+        guess_count = guess_count - 1
+    else:
+        print("Congrats! You've guessed the correct number!")
+        break
+
+if guess_count == 0:
+    print("Game over, you've run out of guesses!")
