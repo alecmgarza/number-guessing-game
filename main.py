906c0e6dcf8d24fa302d58ca11bcfa4f720c048a
Alec Garza <alecmgarza@gmail.com>
2026-01-03 20:30:45 -0600

    Correct a user when entering out of range inputs

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import random
 
-random_num = random.randint(1,100)
-guess_count = 10
-
 def chooseDifficulty():
     global guess_count
 
@@ -21,21 +18,36 @@ def chooseDifficulty():
         print("\nPlease enter a correct key. Do not include spaces.\n\n")
         chooseDifficulty()
 
-chooseDifficulty()
+def guessRounds():
+    global guess_count
 
-player_guess = int(input(f"Thinking of a number between 1 and 100...({guess_count} guesses remaining)\n"))
-guess_count = guess_count - 1
+    player_guess = int(input(f"\nThinking of a number between 1 and 100...({guess_count} guesses remaining)\n\n"))
 
-while guess_count > 0:
-    if player_guess > random_num:
-        player_guess = int(input(f"Lower...({guess_count} guesses remaining)\n"))
-        guess_count = guess_count - 1
-    elif player_guess < random_num:
-        player_guess = int(input(f"Higher...({guess_count} guesses remaining)\n"))
-        guess_count = guess_count - 1
-    else:
-        print("Congrats! You've guessed the correct number!")
-        break
+    while guess_count > 1:
+        if 100 >= player_guess > random_num:
+            guess_count = guess_count - 1
+            player_guess = int(input(f"\nLower...({guess_count} guesses remaining)\n\n"))
+        elif 1 <= player_guess < random_num:
+            guess_count = guess_count - 1
+            player_guess = int(input(f"\nHigher...({guess_count} guesses remaining)\n\n"))
+        elif player_guess > 100 or player_guess < 1:
+            print("\nPlease make sure the number you guess is within range (1-100).\n")
+            guessRounds()
+        else:
+            print("\nCongrats! You've guessed the correct number!")
+            break
 
-if guess_count == 0:
-    print(f"Game over, you've run out of guesses!\nCorrect number was {random_num}.")
+    if guess_count == 1:
+        print(f"\nGame over, you've run out of guesses!\nCorrect number was {random_num}.\n\n")
+        return
+    
+# introduced a bug where multiple out of range inputs result in Game Over message getting called multiple times
+
+def game():
+    global random_num
+
+    chooseDifficulty()
+    random_num = random.randint(1,100)
+    guessRounds()
+
+game()
